19cccf94b1794e699aab5cba9b34544cb362b070
Ajoy Mahanta <joymahanta9@gmail.com>
2022-12-25 20:10:01 +0600

    Removed Database Url

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mon= require("mongoose");
 
 const connectDB= async ()=> {
     try {
-        const url="mongodb+srv://joybd6:test102@cluster0.hihfy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+        const url=process.env.DatabaseUrl
         await mon.connect(url);
         console.log("Database Connected");
     }
